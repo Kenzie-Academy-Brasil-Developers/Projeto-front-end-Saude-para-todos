576c68064870bea5403411e0fcb1cbf5f25f5e98
Mateus9286 <106389123+Mateus9286@users.noreply.github.com>
2023-01-06 19:40:12 -0300

    fix

--- src/Providers/UserContext.tsx
+++ src/Providers/UserContext.tsx
@@ -1,44 +1,39 @@
 import React, { useState } from "react";
-import { Api } from "../services/Api"
-import { toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { useNavigate } from "react-router-dom"
+import { Api } from "../services/Api";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 import { createContext } from "react";
 import { IformData, Iresponse, iUser, iUserContext } from "./@types";
 import { iRegisterFormValues } from "../pages/RegisterPage/interfaceRegister";
-import { iUserContext } from "./@types";
 import { iDefaultProvidersProps, iUserData } from "./@types";
-
 
 export const UserContext = createContext({} as iUserContext);
 export const UserProvider = ({ children }: iDefaultProvidersProps) => {
   const [loading, setLoading] = useState(false);
   const localStorageToken = localStorage.getItem("@SaudeParaTodos");
-  const [userToken, setUserToken] = useState(localStorageToken ? localStorageToken : null);
+  const [userToken, setUserToken] = useState(
+    localStorageToken ? localStorageToken : null
+  );
   const [user, setUser] = useState<iUserData | null>(null);
-
-  const userRegister = () => {};
-
-  const userLogout = () => {};
 
   const navigate = useNavigate();
 
-
   const userRegister = async (formData: iRegisterFormValues) => {
     try {
-      setLoading(true)
-      const response = await Api.post<iUserData>("users", formData)
-      console.log(response)
-      toast.success(`${response.data.user.name.toUpperCase().trim()}, seja bem vindo(a)!`)
-      navigate("/")
+      setLoading(true);
+      const response = await Api.post<iUserData>("users", formData);
+      console.log(response);
+      toast.success(
+        `${response.data.user.name.toUpperCase().trim()}, seja bem vindo(a)!`
+      );
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
-
-
+  };
 
   const userLogout = () => {
     localStorage.removeItem("@SaudeParaTodos");
@@ -46,7 +41,6 @@ export const UserProvider = ({ children }: iDefaultProvidersProps) => {
     setUser(null);
     navigate("/");
   };
-  
 
   const userEdit = () => {};
   const autoLogin = () => {};
