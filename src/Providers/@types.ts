f027a831f2bd676e3d43ad3f607b55140fcdb55d
Mateus9286 <106389123+Mateus9286@users.noreply.github.com>
2023-01-06 11:12:15 -0300

    initial-commit

--- src/Providers/@types.ts
+++ src/Providers/@types.ts
@@ -1,17 +1,28 @@
 import React from "react";
-
 export interface iDefaultProvidersProps {
   children: React.ReactNode;
 }
-
+export interface iUserData {
+  accessToken: string;
+  user: iUser;
+}
+export interface iUser {
+  email: string;
+  name: string;
+  age: number;
+  id: number;
+  isAdmin?: boolean;
+}
 export interface iUserContext {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
   userToken: string | null;
   setUserToken: React.Dispatch<React.SetStateAction<string | null>>;
+  user: iUserData | null;
+  setUser: React.Dispatch<React.SetStateAction<iUserData | null>>;
   userRegister: () => void;
   userLogin: () => void;
   userLogout: () => void;
   userEdit: () => void;
   autoLogin: () => void;
-  loading: boolean;
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
--- src/Providers/UserContext.tsx
+++ src/Providers/UserContext.tsx
@@ -1,39 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { createContext } from "react";
 import { iUserContext } from "./@types";
-import { iDefaultProvidersProps } from "./@types";
+import { iDefaultProvidersProps, iUserData } from "./@types";
 
 export const UserContext = createContext({} as iUserContext);
-
 export const UserProvider = ({ children }: iDefaultProvidersProps) => {
   const [loading, setLoading] = useState(false);
   const localStorageToken = localStorage.getItem("@SaudeParaTodos");
   const [userToken, setUserToken] = useState(
     localStorageToken ? localStorageToken : null
   );
-
+  const [user, setUser] = useState<iUserData | null>(null);
   const userRegister = () => {};
-
   const userLogin = () => {};
-
   const userLogout = () => {};
-
   const userEdit = () => {};
-
   const autoLogin = () => {};
-
   return (
     <UserContext.Provider
       value={{
+        loading,
+        setLoading,
+        userToken,
+        setUserToken,
+        user,
+        setUser,
         userRegister,
         userLogin,
         userLogout,
         userEdit,
         autoLogin,
-        loading,
-        setLoading,
-        userToken,
-        setUserToken,
       }}
     >
       {children}
