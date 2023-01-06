e3ddaea6e3f074a2774de45582abd51feebfea93
Mateus9286 <106389123+Mateus9286@users.noreply.github.com>
2023-01-06 14:07:08 -0300

    feat

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "./App";
-=======
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
->>>>>>> 7cc89720b097cd042654173e9a6b506f8e9e465e
 
 test("renders learn react link", () => {
   render(<App />);
