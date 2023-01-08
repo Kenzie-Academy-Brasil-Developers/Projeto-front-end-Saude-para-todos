7834acf010f4a12d869fed0e2a6ab9702804611a
FRANCISCO ANGHINONI <102160768+FranciscoAnghinoni@users.noreply.github.com>
2023-01-08 10:44:44 -0400

    feat footer

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Footer } from "../../Components/Footer";
 import { Header } from "../../Components/Header";
 
 export const HomePage = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <h1>body</h1>
+      <Footer />
     </div>
   );
 };
