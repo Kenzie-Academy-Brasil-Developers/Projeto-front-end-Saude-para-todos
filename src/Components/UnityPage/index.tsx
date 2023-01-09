b2298feabfb7fd0a3ee94cf35e5eef049ec4abb9
FRANCISCO ANGHINONI <102160768+FranciscoAnghinoni@users.noreply.github.com>
2023-01-09 09:24:52 -0400

    feat unity page
    start

---
+++ src/Components/UnityPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const UnityPage = () => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <p>Nome</p>
+        <p>cep</p>
+        <p>endereço</p>
+        <p>bairro</p>
+        <p>contato</p>
+        <p>link</p>
+    </div>
+  )
+}
